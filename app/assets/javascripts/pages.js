c85f1038373bcce2af825e6970bacd2c15fbcd2e
Ddaniela <ddaniela_dragomir@yahoo.com>
2013-07-03 16:33:51 +0300

    change image to textarea

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -6,15 +6,15 @@ $(document).ready(function() {
   $('a#read-email').click(function(e){
     e.preventDefault();
     e.stopPropagation();
+
     var old_text = $('a#read-email').text();
     $('a#read-email').text('Waiting...');
-
-    var email_text = "Hi George, I was so happy to hear from you again. I hope you enjoyed the gift " +
-      "we've sent you. I can't wait to meet you this summer. Take care, Alice.";
+    
+    var email_text = $("textarea#my-email").val();
 
     $.getJSON("http://speech.jtalkplugin.com/api/?callback=?",
       {
-        speech: email_text,
+        speech: email_text.replace(/[\n\r]/g, ''),
         usecache: "false"
       },
       function(json) {
