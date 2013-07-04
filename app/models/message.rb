e524d520634276e5e70383cc47736ff05f03661f
Andreea <andreea_laura.ungureanu@yahoo.com>
2013-07-04 15:54:38 +0300

    Meniu initial

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  attr_accessible :content, :receiver_id, :sender_id, :subject
+  attr_accessible :content, :receiver_id, :sender_id, :subject, :sender, :recevier
 
   belongs_to :sender, :class_name => 'User', :foreign_key => 'sender_id'
   belongs_to :receiver, :class_name => 'User', :foreign_key => 'receiver_id'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+andrei = User.create!(email: 'andrei@yahoo.com', password: 'aaaaaaaa')
+lavinia = User.create!(email: 'lavinia@yahoo.com', password: 'aaaaaaaa')
+daniela = User.create!(email: 'daniela@yahoo.com', password: 'aaaaaaaa')
+
+users = [andrei, lavinia, daniela]
+30.times do |i|
+	sender = users.sample
+	receiver = (users - [sender]).sample
+
+	message = Message.create!(sender_id: sender.id, receiver_id: receiver.id, subject: "Subject #{i}", content: "Creation is beautiful, lorem ipsum #{i}" )
+end
+
