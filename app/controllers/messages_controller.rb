d0408b71abb1a1f41dec1c2aeb1081906a7cc6bf
Mir <mirosul@gmail.com>
2013-07-22 16:36:37 +0300

    remove auth for messages show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_filter :authenticate_user!, except: [:index]
+  before_filter :authenticate_user!, except: [:index, :show]
   layout 'simple'
 
   # GET /messages
