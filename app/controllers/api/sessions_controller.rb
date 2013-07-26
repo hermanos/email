9cabd6b5a11a667c1bf7e78ebb5c4a4517c0b85d
Bunea <bunea.andrei92@gmail.com>
2013-07-26 15:45:49 +0300

    add rake sync task

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -11,15 +11,6 @@ class Api::SessionsController < Devise::SessionsController
                       :data => { :auth_token => current_user.authentication_token } }
   end
 
-  def destroy
-    warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
-    current_user.update_column(:authentication_token, nil)
-    render :status => 200,
-           :json => { :success => true,
-                      :info => "Logged out",
-                      :data => {} }
-  end
-
   def failure
     render :status => 401,
            :json => { :success => false,
--- app/controllers/api/update_email_controller.rb
+++ app/controllers/api/update_email_controller.rb
@@ -1,12 +1,12 @@
-	require 'net/imap'
+require 'net/imap'
 class Api::UpdateEmailController < ActionController::Base
+	skip_before_filter :verify_authenticity_token	
 	respond_to :json
-
+	
 	def set_email
 		email_address = params[:email_address]
 		email_password = params[:email_password]
-		puts email_address
-		puts email_password
+
 		imap = @imap = Net::IMAP.new('imap.gmail.com',993,true,nil,true)
 		begin
 			imap.authenticate('PLAIN', email_address, email_password)
@@ -14,9 +14,9 @@ class Api::UpdateEmailController < ActionController::Base
 			render json: { success: false, message: "Invalid email or password"}
 			return
 		end
-		current_user.update_attribute(:email_address, email_address)
-		current_user.update_attribute(:email_password, email_password)
-		current_user.update_attribute(:stage, 2)
+		User.first.update_attribute(:email_address, email_address)
+		User.first.update_attribute(:email_password, email_password)
+		User.first.update_attribute(:stage, 2)
 		render json: { success: true, message: "You are now logged in" } 
 	end
 
