bbc69da8847c00267737200a1114efde9bc3352a
Bunea <bunea.andrei92@gmail.com>
2013-07-30 12:01:52 +0300

    new json response

--- app/controllers/api/update_email_controller.rb
+++ app/controllers/api/update_email_controller.rb
@@ -1,23 +1,30 @@
 require 'net/imap'
 class Api::UpdateEmailController < ActionController::Base
 	skip_before_filter :verify_authenticity_token	
-	respond_to :json
+	
 	
 	def set_email
 		email_address = params[:email_address]
 		email_password = params[:email_password]
 
 		imap = @imap = Net::IMAP.new('imap.gmail.com',993,true,nil,true)
-		begin
-			imap.authenticate('PLAIN', email_address, email_password)
-		rescue Net::IMAP::NoResponseError
-			render json: { success: false, message: "Invalid email or password"}
-			return
-		end
-		User.first.update_attribute(:email_address, email_address)
-		User.first.update_attribute(:email_password, email_password)
-		User.first.update_attribute(:stage, 2)
-		render json: { success: true, message: "You are now logged in" } 
+		respond_to do |format|
+      format.json do
+      	# render json: params 
+      	# return
+      	begin
+      		imap.authenticate('PLAIN', email_address, email_password)
+      	rescue Net::IMAP::NoResponseError
+      		render json: { success: false, message: "Invalid email or password"}
+      		return
+      	end
+      	current_user.update_attribute(:email_address, email_address)
+      	current_user.update_attribute(:email_password, email_password)
+      	current_user.update_attribute(:stage, 1)
+      	render json: { success: true, message: "You are now logged in" } 
+      end
+     end
 	end
-
 end
+
+	
