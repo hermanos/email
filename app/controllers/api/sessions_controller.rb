ce1284d6efc251bbdf6ccd54fb92ae3abb790ef0
Bunea <bunea.andrei92@gmail.com>
2013-07-30 22:12:47 +0300

    warden authenticate

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,6 +5,7 @@ class Api::SessionsController < Devise::SessionsController
   def create
     logger.info "intrat in create"
     warden.authenticate!(scope: resource_name)
+    logger.info "dupa warden authenticate"
     respond_to do |format|
       format.json do
         render status: 200,
