cf38acd0c68d3698568e5fb31a22d2b3553914aa
Bunea <bunea.andrei92@gmail.com>
2013-08-01 19:07:29 +0300

    upload files

--- app/controllers/api/upload_controller.rb
+++ app/controllers/api/upload_controller.rb
@@ -2,9 +2,20 @@ class Api::UploadController < ActionController::Base
 	before_filter :verify_authenticity_token
 
 	def create
-		name = params[:upload][:file].original_filename
-		directory = "public/attachments/#{current_user.id}"
-		path = File.join(directory, name)
-		File.open(path, "w+") { |f| f.write(params[:upload][:file].read) }	
+		respond_to do |format|
+			format.json do
+				puts params[:file].inspect
+				puts current_user.id
+				name = params[:file].original_filename
+				directory = "public/attachments/#{current_user.id}"
+				path = File.join(directory, name)
+				unless File.exist?(path)
+					FileUtils.mkdir_p(directory)
+				end
+				File.open(path, "w+") { |f| f.write(params[:file].read) }	
+
+				render json: { success: true }
+			end
+		end
 	end
 end
